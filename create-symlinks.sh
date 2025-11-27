6a224f2ea25502c8f30518abd85d45c7bca1a239
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-11-27 18:19:13 +0100

    add create-symlinks.sh

---
+++ create-symlinks.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${BASEDIR}"
+
+/bin/rm -f ~/.tmux.conf
+ln -sf "${BASEDIR}/tmux.conf" ~/.tmux.conf
+
+/bin/rm -f ~/.alacritty.toml
+ln -sf "${BASEDIR}/alacritty.toml" ~/.alacritty.toml
+
+mkdir -p ~/Library/Application\ Support/com.mitchellh.ghostty/
+/bin/rm -f ~/Library/Application\ Support/com.mitchellh.ghostty/config
+ln -sf "${BASEDIR}/ghostty" ~/Library/Application\ Support/com.mitchellh.ghostty/config
