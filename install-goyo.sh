6aede20ba79cc6deb38a9e02fdb4b678db0243c2
Daniel <39460524+danielfalbo@users.noreply.github.com>
2026-01-04 17:58:06 +0100

    D install-goyo.sh

--- install-goyo.sh
+++
@@ -1,6 +0,0 @@
-# install goyo via vim built-in plugin manager
-
-mkdir -p ~/.vim/pack/plugins/start/
-git clone https://github.com/junegunn/goyo.vim.git ~/.vim/pack/plugins/start/goyo || (cd ~/.vim/pack/plugins/start/goyo && git pull)
-
-
