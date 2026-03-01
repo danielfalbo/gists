493eb3a347cbe0b431449f7a2f483f4925404701
Daniel <39460524+danielfalbo@users.noreply.github.com>
2026-03-01 11:48:09 +0000

    M create-symlinks.sh D ghostty

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -6,10 +6,6 @@ cd "${BASEDIR}"
 /bin/rm -f ~/.alacritty.toml
 ln -sf "${BASEDIR}/alacritty.toml" ~/.alacritty.toml
 
-mkdir -p ~/Library/Application\ Support/com.mitchellh.ghostty/
-/bin/rm -f ~/Library/Application\ Support/com.mitchellh.ghostty/config
-ln -sf "${BASEDIR}/ghostty" ~/Library/Application\ Support/com.mitchellh.ghostty/config
-
 mkdir -p ~/.newsboat/
 /bin/rm -f ~/.newsboat/urls
 ln -sf "${BASEDIR}/urls" ~/.newsboat/urls
